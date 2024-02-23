9e87eaaeb7d411cad30471f2a7247e5c5e2c5289
mazharrasheed <mazharopal@gmail.com>
2024-02-23 09:59:32 +0500

    form fileds settings,messages,form innahartance etc

--- school/student/views.py
+++ school/student/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-
-from .forms import Student0, Student1, Student2,Student_Registerform
+from django.contrib import messages
+from .forms import Student0, Student1, Student2,Student_Registerform,Teacher_Registrations
 from .models import Student 
 
 # Create your views here.
@@ -12,11 +12,12 @@ def index(request):
 
         us=Student.objects.get(pk=1)
         form4=Student_Registerform(request.POST,instance=us)
+        # form4=Student_Registerform(request.POST)
         form4.is_valid()
+
         nm=form4.cleaned_data['username']
         em=form4.cleaned_data['email']
         pw=form4.cleaned_data['password']
-
         form4.save() #for update
 
         print(nm)
@@ -24,22 +25,35 @@ def index(request):
         print(pw)
         reg=Student(username=nm,email=em,password=pw) #for new object
         reg.save()
-
-
-
         form=Student0(request.POST)
         form1=Student1(request.POST)
         form2=Student2(request.POST)
+
         form.order_fields(field_order=['first_name','email','last_name'])
     else:
         form=Student0()
         form1=Student1()
         form2=Student2()
         form4=Student_Registerform()
+
         # form=Student(auto_id=True, label_suffix=" = " ,
         # initial={'first_name':'Mazhar','last_name':'Rasheed'},prefix="asdf")
 
         form.order_fields(field_order=['first_name','email','last_name'])
             
-    data={'form':form ,'form1':form1,'form2':form2,'form4':form4}
+    data={'form':form ,'form1':form1,'form2':form2,'form4':form4,}
+    return render(request,"student/index.html",data)
+
+def index_teacher(request):
+    data={}
+    if request.method=='POST':
+        form5=Teacher_Registrations(request.POST)
+        # form5.is_valid()
+        form5.save()
+        form5=Teacher_Registrations()
+        messages.add_message(request ,messages.INFO ,"Now you can log in") # both methods are working
+        messages.success(request ,"Your account succesfuly created")# both methods are working generaly we use this
+    else:
+        form5=Teacher_Registrations()
+    data={'form5':form5}
     return render(request,"student/index.html",data)
